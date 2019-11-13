6c6525af629bba591d6f758a67e2ca5d5e0b406e
Patrick Rashleigh <edward_rashleigh@brown.edu>
2019-11-13 18:11:25 -0500

    Update demonstration poll Part

--- dist/glider.js
+++ dist/glider.js
@@ -3979,40 +3979,60 @@
 
   // PART VIEWS
 
-  let partView_counterButton = {
+  // Poll form
+
+  const partView_counterButton = {
     partviewName: 'controller',
     template: `
-    <div style="color: black">
-      <h1>I know how to code:</h1>
-      <button type="button" class="btn btn-light btn-block" v-on:click="inc">HTML</button>
-      <button type="button" class="btn btn-light btn-block" v-on:click="inc2">CSS</button>
-      <button type="button" class="btn btn-light btn-block" v-on:click="inc3">Javascript</button>
+    <div>
+      <h1>Choose:</h1>
+      <button type="button" class="btn btn-light btn-block" style="background: #f2e3c9" v-on:click="inc">Yellow</button>
+      <button type="button" class="btn btn-light btn-block" style="background: #ec8f6a" v-on:click="inc2">Orange</button>
+      <button type="button" class="btn btn-light btn-block" style="background: #ef4b4b" v-on:click="inc3">Red</button>
     </div>
   `,
     
     methods: {
-      inc: function() { 
-        console.log('INCREMENT 1!');
-        this.count++;
-      },
+      inc: function() { this.count++; },
       inc2: function() { this.count2++; },
       inc3: function() { this.count3++; }
     }
   };
 
-  let partView_countDisplay = {
+  // Results
+
+  const partView_countDisplay = {
     partviewName: 'display',
     template: `
     <div>
-      <p style="color: red">{{ '■'.repeat(count) }} HTML</p>
-      <p style="color: yellow">{{ '■'.repeat(count2) }} CSS</p>
-      <p style="color: blue">{{ '■'.repeat(count3) }} Javascript</p>
+      <h1>Poll Results</h1>
+      <p style="color: #f2e3c9">{{ '■'.repeat(count) }} Yellow</p>
+      <p style="color: #ec8f6a">{{ '■'.repeat(count2) }} Orange</p>
+      <p style="color: #ef4b4b">{{ '■'.repeat(count3) }} Red</p>
     </div>`
   };
 
-  // WRAP IT ALL UP AND EXPORT
+  // Poll reset button
 
-  let views$2 = [partView_counterButton, partView_countDisplay];
+  const partView_reset = {
+    partviewName: 'reset',
+    template: `
+    <div>
+      <h1>Reset Poll</h1>
+      <button type="button" class="btn btn-light btn-block" v-on:click="reset">
+        Reset
+      </button>
+    </div>`,
+    methods: {
+      reset: function() {
+        this.count = 0;
+        this.count2 = 0;
+        this.count3 = 0;
+      }
+    }
+  };
+
+  const views$2 = [partView_counterButton, partView_reset, partView_countDisplay];
 
   var poll = /*#__PURE__*/Object.freeze({
     part: part$2,
