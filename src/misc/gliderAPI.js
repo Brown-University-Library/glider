2f77bd5ac69f0e266824f6c0695d64dddbd22952
Patrick Rashleigh <edward_rashleigh@brown.edu>
2020-07-13 16:36:28 -0400

    Refactor 2020 - API through global Glider object is encapsulated in its own module

---
+++ src/misc/gliderAPI.js
@@ -0,0 +1,12 @@
+
+
+// This is the global object that the user interacts with
+
+export function getGliderAPI({gliderApp, parts, phases, places}) {
+  return {
+    parts: parts,
+    phases: phases,
+    places: places
+  }
+}
+
