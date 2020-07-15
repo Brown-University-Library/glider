7b28d47b89b54b7930d673a0220cac5eeb7b6678
Patrick Rashleigh <edward_rashleigh@brown.edu>
2020-07-15 08:42:01 -0400

    Develop glider API a bit more; implement DEBUG mode

--- src/misc/gliderAPI.js
+++ src/misc/gliderAPI.js
@@ -1,12 +1,23 @@
 
-
 // This is the global object that the user interacts with
 
-export function getGliderAPI({gliderApp, parts, phases, places}) {
-  return {
-    parts: parts,
-    phases: phases,
-    places: places
-  }
-}
+export function getGliderAPI(options) {
 
+  let gliderObject = {
+    parts: options.parts,
+    phases: options.phases,
+    places: options.places,
+  };
+
+  // Only set these if in debug mode
+
+  if (options.DEBUG) {
+    gliderObject = Object.assign(gliderObject, {
+      displayDomRoot: options.displayDomRoot,
+      sharedStore: options.sharedStore,
+      sharedStoreConn: options.sharedStoreConn
+    });
+  }
+
+  return gliderObject;
+}
